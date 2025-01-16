c560c0d74d8134c17fc4cbb4a7ba71eea8f7c2f9
Evan Musick <musickevan1@gmail.com>
2025-01-16 14:40:27 -0600

    Fix index.html path resolution in Vite config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     copyPublicDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, './index.html')
+        main: resolve(__dirname, 'index.html')
       },
       output: {
         entryFileNames: '[name].js',
