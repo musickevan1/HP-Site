d972f1100e31bea4193dbe4fa8dde6b07182bac2
Evan Musick <musickevan1@gmail.com>
2025-01-16 14:38:57 -0600

    Restore index.html to root and update Vite config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './public',
+  root: './',
   base: '/',
   plugins: [react()],
   optimizeDeps: {
@@ -24,10 +24,10 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    copyPublicDir: false,
+    copyPublicDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, './public/index.html')
+        main: resolve(__dirname, './index.html')
       },
       output: {
         entryFileNames: '[name].js',
